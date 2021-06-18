bdac0c158e320277d1b377b2f4a7b97d29921e82
Artem Ugryumov <artiom.ygryumov@gmail.com>
2021-06-18 16:31:51 +0300

    Сделал модальные окна

--- js/main.js
+++ js/main.js
@@ -67,3 +67,97 @@ function closeMenuWhenClickBg() {
     }
   })
 }
+
+// Модальные окна
+const modalOpenElems = document.querySelectorAll('[data-modal-open]'),
+      modalBg = document.querySelector('.modals-bg'),
+      body = document.querySelector('body');
+
+modalOpenElems.forEach(modalOpen => {
+  modalOpen.addEventListener('click', (e) => {
+    e.stopPropagation();
+    
+    const modalOpenData = modalOpen.dataset.modalOpen,
+          modal = document.querySelector(`[data-modal=${modalOpenData}]`);
+
+    modal.classList.add('_show');
+    modalBg.classList.add('_show');
+    body.classList.add('_lock');
+
+    closeModalWithClickBg(modal, modalOpen);
+    closeModalWithClickClose(modal);
+  })
+})
+
+function closeModalWithClickBg(modal, modalOpen) {
+  document.addEventListener('click', (e) => {
+    if (modal.classList.contains('_show')) {
+        const target = e.target,
+            itsModal = target == modal || modal.contains(target),
+            isBtnModalOpen = target == modalOpen,
+            modalShow = modal.classList.contains('_show');
+      
+      // console.log(itsModal,isBtnModalOpen)
+
+      if (!itsModal && !isBtnModalOpen && modalShow) {
+        console.log('ok')
+        modalBg.classList.remove('_show');
+        modal.classList.remove('_show');
+        body.classList.remove('_lock');
+        return false;
+      }
+    }
+  })
+}
+
+function closeModalWithClickClose(modal) {
+  const modalClose = modal.querySelector('.modal-close');
+
+  modalClose.addEventListener('click', () => {
+    modalBg.classList.remove('_show');
+    modal.classList.remove('_show');
+    body.classList.remove('_lock');
+  })
+}
+
+// Маска для инпута телефона
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		console.log(template);
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
+	
+}
+
+maskPhone(".mask");
